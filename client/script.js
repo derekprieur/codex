cabc3292b3d72c072fa0e62a5bec8b93a46ecdbe
derekprieur <DPrieur123@gmail.com>
2022-12-23 10:01:06 -0500

    updated server url

--- client/script.js
+++ client/script.js
@@ -78,7 +78,7 @@ const handleSubmit = async (e) => {
   loader(messageDiv);
 
   // fetch data from server -> bot's response
-  const response = await fetch('https://codex-9qys.onrender.com/', {
+  const response = await fetch('https://codex-a3hr.onrender.com/', {
     method: 'POST',
     headers: {
       'Content-Type': 'application/json'
